63d5a2c50bc0a4037cb9417d10f49469c3647b36
mtmccarthy <mtmccarthy@wpi.edu>
2018-02-02 22:48:50 -0500

    Now tracks home to work and work to home pricing

--- OptimalUberRides/main.py
+++ OptimalUberRides/main.py
@@ -38,7 +38,8 @@ def main():
     franklin_street = (42.4755587, -71.0897706)
     silicon_labs = (42.3506141, -71.0515498)
 
-    prices = get_price_estimate(franklin_street[0], franklin_street[1], silicon_labs[0], silicon_labs[1])
+    home_to_work_prices = get_price_estimate(franklin_street[0], franklin_street[1], silicon_labs[0], silicon_labs[1])
+    work_to_home_prices = get_price_estimate(silicon_labs[0], silicon_labs[1], franklin_street[0], franklin_street[1])
 
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client-secret.json', scope)
@@ -46,14 +47,25 @@ def main():
 
     sheet = client.open('Uber Pricing').sheet1
 
+    #Home to Work
     num_rows = len(sheet.get_all_records()) #0 means no data, but labels in first row
     strRowNum = str((num_rows + 2))
 
     sheet.update_acell('A'+ strRowNum, datetime.now())
     sheet.update_acell('B'+strRowNum, "Home")
     sheet.update_acell('C' + strRowNum, "Work")
-    sheet.update_acell('D' + strRowNum, prices['uberx'][0])
-    sheet.update_acell('E' + strRowNum, prices['uberx'][1])
+    sheet.update_acell('D' + strRowNum, home_to_work_prices['uberx'][0])
+    sheet.update_acell('E' + strRowNum, home_to_work_prices['uberx'][1])
+
+    num_rows = len(sheet.get_all_records()) #0 means no data, but labels in first row
+    strRowNum = str((num_rows + 2))
+
+    #Work to Home
+    sheet.update_acell('A'+ strRowNum, datetime.now())
+    sheet.update_acell('B'+strRowNum, "Work")
+    sheet.update_acell('C' + strRowNum, "Home")
+    sheet.update_acell('D' + strRowNum, work_to_home_prices['uberx'][0])
+    sheet.update_acell('E' + strRowNum, work_to_home_prices['uberx'][1])
 
 if __name__ == '__main__':
     main()
