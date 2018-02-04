0d93ed06b674677b6d7c87505e98aa9827ce544a
mtmccarthy <matthew.mccarthy@silabs.com>
2018-02-04 17:25:47 -0500

    Renamed file and added a test file

---
+++ OptimalUberRides/optimal_rides_tests.py
@@ -0,0 +1,33 @@
+from optimal_rides import *
+import unittest
+
+
+class TestValidateSpreadsheet(unittest.TestCase):
+    def setUp(self):
+        maps_key = "AIzaSyC4N2CrBVCXL9IXAJJim-Rn5xLM3sl-d2s"
+        # Restricted key, can only be run from my server.
+        self.gmaps = googlemaps.Client(key=maps_key)
+        self.franklin_str = '152 Franklin St, Stoneham, MA'
+        self.silicon_str = '343 Congress St, Boston, MA'
+        self.franklin_street = self.gmaps.geocode(self.franklin_str)
+        self.silicon_labs = self.gmaps.geocode(self.silicon_str)
+
+    def test_gmaps_geocode(self):
+        franklin_lat = 42.4754656
+        franklin_long = -71.0898005
+        self.assertAlmostEqual(franklin_lat,
+                               self.franklin_street[0], delta=.00001)
+        self.assertAlmostEqual(franklin_long, self.franklin_street[1], delta=.0001)
+
+        
+class TestPriceEstimate(unittest.TestCase):
+    pass
+
+
+class TestDurationEstimate(unittest.TestCase):
+    pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
